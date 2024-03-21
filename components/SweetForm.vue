11c44bc8f7f2c9ae32e20969fadc63bd3b7222d3
EGRrqq <egrrqqdev@gmail.com>
2024-03-21 09:43:46 -0400

    feat: form template

--- components/SweetForm.vue
+++ components/SweetForm.vue
@@ -2,6 +2,23 @@
 
 <template>
   <form>
-    <input placeholder="buh" type="search" />
+    <fieldset>
+      <legend>sign up</legend>
+
+      <section>
+        <label for="username">username</label>
+        <input id="username" type="text" required />
+      </section>
+      <section>
+        <label for="email">email</label>
+        <input id="email" type="email" required />
+      </section>
+      <section>
+        <label for="password">password</label>
+        <input id="password" type="password" required />
+      </section>
+
+      <button>confirm</button>
+    </fieldset>
   </form>
 </template>
