fddc8ca562125127d22fafd4e228c1d9e2753045
EGRrqq <egrrqqdev@gmail.com>
2024-03-22 17:11:29 -0400

    feat: test bg pattern

--- components/SweetForm.vue
+++ components/SweetForm.vue
@@ -2,91 +2,115 @@
 
 <style>
 :root {
-  --form-gap: 0.25rem;
+  --form-gap: 1rem;
 }
 
 form {
   background-color: white;
   box-shadow: 0.25px 0.25px 0 0px rgba(0, 0, 0, 0.75),
     0.5px 0.5px 0 0px rgba(0, 0, 0, 0.5);
-
-  border: black solid;
-  border-radius: 0.05rem;
-  padding: var(--form-gap) 0;
 }
 
 input {
-  border: solid;
+  border-left: solid;
+  border-right: solid;
 }
 
 fieldset {
   display: grid;
-  gap: var(--form-gap);
 }
 
 .legend {
   background: lightblue;
-  border: solid;
   text-align: center;
+
+  flex-grow: 1;
 }
 
 .input-wrapper {
   display: grid;
-  border: solid;
-  padding: var(--form-gap);
+  padding: 0 var(--form-gap);
   background: lightyellow;
+
+  border-left: solid;
+  border-right: solid;
+}
+
+.block-bg_separator {
+  background:
+    radial-gradient(circle at center, #051c56 50%, transparent 1%),
+    linear-gradient(90deg, #051c56 1px, transparent 1px);
+}
+
+.block-bg {
+  background:
+    radial-gradient(circle at center, #051c56 50%, transparent 1%);
 }
 
 .block {
-  background: radial-gradient(circle at center, #000 50%, transparent 1%), white;
-  background-size: 1rem 1rem;
-  background-position: center;
-  background-repeat: repeat-x;
+  background-size: var(--form-gap) var(--form-gap);
+  background-position: 0 center;
+  background-repeat: space;
+
+  min-height: var(--form-gap);
 }
 
 #confirm-btn {
   background: salmon;
-  border: solid;
+
+  flex-grow: 1;
 }
 
 .row {
-  display: grid;
+  display: flex;
   padding: 0 var(--form-gap);
+  border: solid;
 }
 </style>
 
 <template>
   <form>
     <fieldset>
-      <!-- <legend>sign up</legend> -->
-      <div class="row block">
+      <div class="row block block-bg_separator"></div>
+
+      <div class="row block block-bg">
         <section class="legend">
           <h4>sign up</h4>
         </section>
       </div>
 
-      <div class="row block">
+      <div class="row block block-bg_separator"></div>
+
+      <div class="row block block-bg">
         <section class="input-wrapper">
-          <!-- <label for="username">username</label> -->
           <input id="username" placeholder="username" type="text" required />
         </section>
       </div>
-      <div class="row block">
+
+      <div class="row block block-bg_separator"></div>
+
+      <div class="row block block-bg">
         <section class="input-wrapper">
-          <!-- <label for="email">email</label> -->
           <input id="email" placeholder="email" type="email" required />
         </section>
       </div>
-      <div class="row block">
+
+      <div class="row block block-bg_separator"></div>
+
+      <div class="row block block-bg">
         <section class="input-wrapper">
-          <!-- <label for="password">password</label> -->
           <input id="password" placeholder="password" type="password" required />
         </section>
       </div>
 
-      <div class="row block">
+      <div class="row block block-bg_separator"></div>
+
+      <div class="row block block-bg">
         <button id="confirm-btn">confirm</button>
       </div>
+
+      <div class="row block block-bg_separator"></div>
+
     </fieldset>
   </form>
 </template>
