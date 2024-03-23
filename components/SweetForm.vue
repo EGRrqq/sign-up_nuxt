246e6ac67e15ab30618b670d8df29833fe65fc37
EGRrqq <egrrqqdev@gmail.com>
2024-03-23 10:01:22 -0400

    feat: input block

--- components/SweetForm.vue
+++ components/SweetForm.vue
@@ -3,43 +3,30 @@
 <style>
 :root {
   --form-gap: 1rem;
+  --form-border: 0.05rem;
 }
 
 form {
   background-color: white;
-  box-shadow: 0.25px 0.25px 0 0px rgba(0, 0, 0, 0.75),
-    0.5px 0.5px 0 0px rgba(0, 0, 0, 0.5);
 }
 
 input {
-  border-left: solid;
-  border-right: solid;
+  min-height: calc(var(--form-gap) * 2);
 }
 
 fieldset {
   display: grid;
 }
 
-.legend {
-  background: lightblue;
-  text-align: center;
-
-  flex-grow: 1;
-}
-
 .input-wrapper {
   display: grid;
-  padding: 0 var(--form-gap);
-  background: lightyellow;
 
-  border-left: solid;
-  border-right: solid;
-}
+  border: var(--form-border) solid transparent;
+  border-top: 0;
+  border-bottom: 0;
 
-.block-bg_separator {
-  background:
-    radial-gradient(circle at center, #051c56 50%, transparent 1%),
-    linear-gradient(90deg, #051c56 1px, transparent 1px);
+  /* padding: 0 var(--form-gap); */
+  background: olivedrab;
 }
 
 .block-bg {
@@ -47,70 +34,42 @@ fieldset {
     radial-gradient(circle at center, #051c56 50%, transparent 1%);
 }
 
-.block {
-  background-size: var(--form-gap) var(--form-gap);
-  background-position: 0 center;
-  background-repeat: space;
-
-  min-height: var(--form-gap);
+.block-bg_separator {
+  background:
+    radial-gradient(circle at center, #051c56 50%, transparent 1%),
+    linear-gradient(90deg, #051c56 var(--form-border), transparent 1%),
+    linear-gradient(270deg, #051c56 var(--form-border), transparent 1%),
+    linear-gradient(-180deg, #051c56 var(--form-border), transparent 1%),
+    linear-gradient(360deg, #051c56 var(--form-border), transparent 1%);
 }
 
-#confirm-btn {
-  background: salmon;
+.block {
+  background-size: var(--form-gap) var(--form-gap);
+  /* background-position: 0 center; */
+  background-repeat: round;
 
-  flex-grow: 1;
+  min-height: var(--form-gap);
 }
 
 .row {
   display: flex;
   padding: 0 var(--form-gap);
-  border: solid;
+  outline: var(--form-border) solid;
 }
 </style>
 
 <template>
   <form>
     <fieldset>
-      <div class="row block block-bg_separator"></div>
-
-      <div class="row block block-bg">
-        <section class="legend">
-          <h4>sign up</h4>
-        </section>
+      <div>
+        <div class="row block block-bg_separator"></div>
+        <div class="row block block-bg_separator">
+          <section class="input-wrapper">
+            <input id="password" placeholder="password" type="password" required />
+          </section>
+        </div>
+        <div class="row block block-bg_separator"></div>
       </div>
-
-      <div class="row block block-bg_separator"></div>
-
-      <div class="row block block-bg">
-        <section class="input-wrapper">
-          <input id="username" placeholder="username" type="text" required />
-        </section>
-      </div>
-
-      <div class="row block block-bg_separator"></div>
-
-      <div class="row block block-bg">
-        <section class="input-wrapper">
-          <input id="email" placeholder="email" type="email" required />
-        </section>
-      </div>
-
-      <div class="row block block-bg_separator"></div>
-
-      <div class="row block block-bg">
-        <section class="input-wrapper">
-          <input id="password" placeholder="password" type="password" required />
-        </section>
-      </div>
-
-      <div class="row block block-bg_separator"></div>
-
-      <div class="row block block-bg">
-        <button id="confirm-btn">confirm</button>
-      </div>
-
-      <div class="row block block-bg_separator"></div>
-
     </fieldset>
   </form>
 </template>
